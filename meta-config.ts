d268944c4ba581f82e76a4327c9e6a40ee75daec
Jayanidu Kaushalya <jayanidukaushalya@gmail.com>
2024-10-06 07:33:19 +0530

    fix: reverse http config

--- meta-config.ts
+++
@@ -1,9 +0,0 @@
-export const initializeMixedContent = () => {
-  if (process.env.NODE_ENV === "development") {
-    // Create a meta tag to allow mixed content
-    const meta = document.createElement("meta");
-    meta.httpEquiv = "Content-Security-Policy";
-    meta.content = "upgrade-insecure-requests";
-    document.getElementsByTagName("head")[0].appendChild(meta);
-  }
-};
